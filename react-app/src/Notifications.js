71eb0dbad3f13b6d8be66f669b4b21fd79f4c823
Raul <raul.antonio.fernandez1@gmail.com>
2023-03-19 02:53:08 -0600

    design single notification element

---
+++ react-app/src/Notifications.js
@@ -0,0 +1,26 @@
+import avatar from "./images/avatar/avatar-angela-gray.webp"
+
+function Notifications() {
+  let Notifications = (
+    <figure className="my-2 md:flex bg-slate-100 rounded-md py-4  dark:bg-slate-600 text-base">
+      <img className="   h-12 w-auto m-auto rounded-full" src={avatar} alt="" width="384" height="512" />
+      <div className=" my-auto text-center md:text-left  w-[90%]">
+        <blockquote>
+          <p className="text-lg ">
+            <span className="font-semibold">Angela Gray</span> followed you
+          </p>
+        </blockquote>
+        <figcaption className="">
+          <div className="text-sky-500 dark:text-sky-400">
+            5m ago
+          </div>
+        </figcaption>
+      </div>
+    </figure>
+  )
+  return (
+    <div>{Notifications}{Notifications}</div>
+  );
+}
+
+export default Notifications;
---
+++ react-app/src/NotificationsContainer.js
@@ -0,0 +1,12 @@
+import './index.css';
+import Notifications from './Notifications';
+
+function NotificationsContainer() {
+    return (
+        <div className="p-5 container mx-auto w-1/2">
+            <Notifications />
+        </div>
+    );
+}
+
+export default NotificationsContainer;
